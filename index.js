7592a46a33d9e9c7322633b8fb315ce5a6512fad
palash <palash@spoilz.studio>
2024-04-28 08:50:38 +0530

    socket connection, emitting events

---
+++ index.js
@@ -0,0 +1,37 @@
+
+const path = require('path');
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+let users = 0;
+
+app.get('/', (req, res) => {
+    var options = {
+        root: path.join(__dirname)
+    };
+    var filename = 'index.html';
+    res.sendFile(filename, options);
+});
+
+io.on('connection', (socket) => {
+    users++;
+    console.log('A user connected');
+
+    io.emit('broadcast', {description: users + ' connected'});
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+    socket.on('dEvent', (data)=>{
+        console.log(data);
+    })
+});
+
+server.listen(3000, () => {
+    console.log('Server started on port 3000');
+});
